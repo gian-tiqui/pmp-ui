d60c31b1987ebb665285114f4c46fd1fe73fd0e4
Michael Gian Tiqui <Michael.Tiqui@WMC.local>
2024-11-25 14:03:15 +0800

    board and gantt integration done, fix the fixed positioning

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,16 +1,21 @@
-import { useEffect, useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { CustomTask, TaskListType } from "../types/types";
+import useTasksStore from "../store/tasks";
 
 interface Props {
   tasks: CustomTask[];
+  onTaskDrop?: (taskId: string, newGroup: string) => void;
 }
 
-const TaskList: React.FC<Props> = ({ tasks }) => {
+const TaskList: React.FC<Props> = ({ tasks, onTaskDrop }) => {
   const [groupedTasks, setGroupedTasks] = useState<TaskListType[]>([
     { name: "todo", tasks: [] },
     { name: "in-progress", tasks: [] },
     { name: "done", tasks: [] },
   ]);
+  const { setTasks } = useTasksStore();
+
+  const draggedItemRef = useRef<CustomTask | null>(null); // Ref to store the dragged task
 
   useEffect(() => {
     const groupTaskByStatus = () => {
@@ -36,6 +41,74 @@ const TaskList: React.FC<Props> = ({ tasks }) => {
     groupTaskByStatus();
   }, [tasks]);
 
+  const handleDragStart = (
+    task: CustomTask,
+    event: React.DragEvent<HTMLDivElement>
+  ) => {
+    draggedItemRef.current = task;
+    event.dataTransfer.setData("text/plain", task.id); // Set task ID for drag data
+  };
+
+  const handleDrop = (
+    event: React.DragEvent<HTMLDivElement>,
+    targetGroup: string
+  ) => {
+    event.preventDefault();
+
+    const droppedTaskId = event.dataTransfer.getData("text/plain");
+    if (!droppedTaskId || !draggedItemRef.current) return; // Handle invalid drop
+
+    const updatedGroupedTasks = [...groupedTasks];
+    const sourceGroupIndex = updatedGroupedTasks.findIndex((group) =>
+      group.tasks.find((t) => t.id === droppedTaskId)
+    );
+    const targetGroupIndex = updatedGroupedTasks.findIndex(
+      (group) => group.name === targetGroup
+    );
+
+    if (
+      sourceGroupIndex !== -1 &&
+      targetGroupIndex !== -1 &&
+      sourceGroupIndex !== targetGroupIndex
+    ) {
+      // Remove task from source group
+      const sourceGroup = updatedGroupedTasks[sourceGroupIndex];
+      const sourceTasks = sourceGroup.tasks.filter(
+        (t) => t.id !== droppedTaskId
+      );
+      sourceGroup.tasks = sourceTasks;
+
+      // Add task to target group and update progress
+      const draggedTask = draggedItemRef.current as CustomTask;
+      switch (targetGroup) {
+        case "todo":
+          draggedTask.progress = 0;
+          break;
+        case "done":
+          draggedTask.progress = 100;
+          break;
+        case "in-progress":
+          draggedTask.progress = 50;
+          break;
+        default:
+          break;
+      }
+      updatedGroupedTasks[targetGroupIndex].tasks.push(draggedTask);
+      setGroupedTasks(updatedGroupedTasks);
+
+      const newTasks = [...updatedGroupedTasks.flatMap((group) => group.tasks)];
+
+      setTasks(newTasks);
+
+      if (!onTaskDrop) return;
+      // Call prop callback to update state in parent component (if needed)
+      onTaskDrop(droppedTaskId, targetGroup); // Optional: Inform parent about drop
+    }
+  };
+  const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault(); // Allow drop on target element
+  };
+
   return (
     <div>
       <h1 className="pt-5 pb-5 text-xl font-semibold text-center">
@@ -43,11 +116,22 @@ const TaskList: React.FC<Props> = ({ tasks }) => {
       </h1>
       <div className="grid w-full grid-cols-3 gap-1">
         {groupedTasks.map((groupedTask, index) => (
-          <div key={index} className="w-full bg-white h-52">
+          <div
+            key={index}
+            className="w-full p-2 bg-white h-52"
+            onDrop={(event) => handleDrop(event, groupedTask.name)}
+            onDragOver={handleDragOver}
+          >
             <h1>{groupedTask.name}</h1>
             <div className="grid">
-              {groupedTask.tasks.map((task, taskIndex) => (
-                <p key={taskIndex}>{task.name}</p>
+              {groupedTasks[index].tasks.map((task, taskIndex) => (
+                <p
+                  key={taskIndex}
+                  draggable
+                  onDragStart={(event) => handleDragStart(task, event)}
+                >
+                  {task.name}
+                </p>
               ))}
             </div>
           </div>
